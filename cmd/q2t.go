81649cfedd4303d00d9cf8b97a7027a9e7f80e30
flano-yuki <minami.hiroy@gmail.com>
2019-08-22 13:43:21 +0900

    Multiplexing with streams on a quic connection

--- cmd/q2t.go
+++ cmd/q2t.go
@@ -7,20 +7,20 @@ import (
 	"net"
 	"os"
 
-	"github.com/spf13/cobra"
-	quic "github.com/lucas-clemente/quic-go"
-	"github.com/flano-yuki/t2q2t/lib"
 	"github.com/flano-yuki/t2q2t/config"
+	"github.com/flano-yuki/t2q2t/lib"
+	quic "github.com/lucas-clemente/quic-go"
+	"github.com/spf13/cobra"
 )
 
 var q2tCmd = &cobra.Command{
 	Use:   "q2t",
 	Short: "Listen by quic, and forward to tcp",
-	Long:  `Listen by quic, and forward to tcp
+	Long: `Listen by quic, and forward to tcp
   t2q2t q2t <Listen Addr> <forward Addr>  
 
   go run ./t2q2t.go q2t 0.0.0.0:2022 127.0.0.1:22`,
-	Args:  cobra.MinimumNArgs(2),
+	Args: cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("q2t called")
 		listen := args[0]
@@ -39,8 +39,8 @@ func runq2t(listen, to string) {
 	addr := listen
 	fmt.Printf("Listen QUIC on: %s \n", addr)
 
-  tlsConfig := config.GenerateServerTLSConfig()
-  quicConfig := config.GenerateServerQUICConfig()
+	tlsConfig := config.GenerateServerTLSConfig()
+	quicConfig := config.GenerateServerQUICConfig()
 	listener, err := quic.ListenAddr(addr, tlsConfig, quicConfig)
 	toTcpAddr, err := net.ResolveTCPAddr("tcp4", to)
 	if err != nil {
@@ -51,16 +51,22 @@ func runq2t(listen, to string) {
 		if err != nil {
 			os.Exit(0)
 		}
-		stream, err := sess.AcceptStream(context.Background())
-		if err != nil {
-			os.Exit(0)
-		}
-		go q2tHandleConn(stream, toTcpAddr)
+		go q2tHandleConn(sess, toTcpAddr)
 	}
 
 }
 
-func q2tHandleConn(stream quic.Stream, toTcpAddr *net.TCPAddr) error {
+func q2tHandleConn(sess quic.Session, toTcpAddr *net.TCPAddr) error {
+	for {
+		stream, err := sess.AcceptStream(context.Background())
+		if err != nil {
+			return err
+		}
+		go q2tHandleStream(stream, toTcpAddr)
+	}
+}
+
+func q2tHandleStream(stream quic.Stream, toTcpAddr *net.TCPAddr) error {
 	fmt.Printf("Connect TCP to: %s \n", toTcpAddr.String())
 	conn, err := net.DialTCP("tcp", nil, toTcpAddr)
 	if err != nil {
@@ -77,4 +83,3 @@ func q2tHandleConn(stream quic.Stream, toTcpAddr *net.TCPAddr) error {
 
 	return nil
 }
-
--- cmd/t2q.go
+++ cmd/t2q.go
@@ -3,23 +3,23 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"github.com/flano-yuki/t2q2t/config"
+	"github.com/flano-yuki/t2q2t/lib"
+	quic "github.com/lucas-clemente/quic-go"
+	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
 	"net"
 	"os"
-	"golang.org/x/sync/errgroup"
-	"github.com/spf13/cobra"
-	quic "github.com/lucas-clemente/quic-go"
-	"github.com/flano-yuki/t2q2t/lib"
-	"github.com/flano-yuki/t2q2t/config"
 )
 
 var t2qCmd = &cobra.Command{
 	Use:   "t2q",
 	Short: "Listen by tcp, and forward to quic",
-	Long:  `Listen by tcp, and forward to quic
+	Long: `Listen by tcp, and forward to quic
   t2q2t t2q <Listen Addr> <forward Addr>  
 
   go run ./t2q2t.go t2q 0.0.0.0:2022 127.0.0.1:2022:`,
-	Args:  cobra.MinimumNArgs(2),
+	Args: cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("t2q called")
 		listen := args[0]
@@ -42,29 +42,32 @@ func runt2q(listen, to string) {
 	fmt.Printf("Listen TCP on: %s \n", listenTcpAddr.String())
 
 	lt, err := net.ListenTCP("tcp", listenTcpAddr)
+
+	tlsConf := config.GenerateClientTLSConfig()
+	quicConf := config.GenerateClientQUICConfig()
+	var sess quic.Session = nil
 	for {
 		conn, err := lt.AcceptTCP()
 		if err != nil {
 			os.Exit(0)
 		}
-		go t2qHandleConn(conn, toTcpAddr)
+
+    // TODO
+    // and, if connection has closed
+		if sess == nil {
+			sess, err = quic.DialAddr(toTcpAddr.String(), tlsConf, quicConf)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Connect QUIC to: %s \n", toTcpAddr.String())
+		}
+		go t2qHandleConn(conn, sess)
 	}
 }
 
-func t2qHandleConn(conn *net.TCPConn, toTcpAddr *net.TCPAddr) error {
-	fmt.Printf("Connect QUIC to: %s \n", toTcpAddr.String())
-
+func t2qHandleConn(conn *net.TCPConn, sess quic.Session) error {
 	var stream quic.Stream
-
-	tlsConf := config.GenerateClientTLSConfig()
-	quicConf := config.GenerateClientQUICConfig()
-	sess, err := quic.DialAddr(toTcpAddr.String(), tlsConf, quicConf)
-	if err != nil {
-		return err
-	}
-
-	stream, err = sess.OpenStreamSync(context.Background())
-
+	stream, err := sess.OpenStreamSync(context.Background())
 	if err != nil {
 		panic(err)
 	}
