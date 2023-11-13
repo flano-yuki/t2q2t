181a8063b94e1a388c5992de03c228ebc0e4634a
oniyan <82947950+oniyan@users.noreply.github.com>
2023-11-14 04:53:21 +0900

    Update q2t.go

--- cmd/q2t.go
+++ cmd/q2t.go
@@ -3,81 +3,78 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"golang.org/x/sync/errgroup"
-	"net"
-
 	"github.com/oniyan/t2q2t/config"
 	"github.com/oniyan/t2q2t/lib"
-	quic "github.com/oniyan/t2q2t/quicgo"
+	quic "github.com/quic-go/quic-go"
 	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
+	"net"
 )
 
-var q2tCmd = &cobra.Command{
-	Use:   "q2t",
-	Short: "Listen by quic, and forward to tcp",
-	Long: `Listen by quic, and forward to tcp
-  t2q2t q2t <Listen Addr> <forward Addr>  
+var t2qCmd = &cobra.Command{
+	Use:   "t2q",
+	Short: "Listen by tcp, and forward to quic",
+	Long: `Listen by tcp, and forward to quic
+  t2q2t t2q <Listen Addr> <forward Addr>  
 
-  go run ./t2q2t.go q2t 0.0.0.0:2022 127.0.0.1:22`,
+  go run ./t2q2t.go t2q 0.0.0.0:2022 127.0.0.1:2022:`,
 	Args: cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("q2t called")
+		fmt.Println("t2q called")
 		listen := args[0]
 		to := args[1]
 
-		err := runq2t(listen, to)
+		err := runt2q(listen, to)
 		if err != nil {
-			fmt.Printf("Error %s\n", err)
+			fmt.Printf("[Error] %s\n", err)
 		}
-
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(q2tCmd)
+	rootCmd.AddCommand(t2qCmd)
 }
 
-func runq2t(listen, to string) error {
-	addr := listen
-	fmt.Printf("Listen QUIC on: %s \n", addr)
-
-	tlsConfig := config.GenerateServerTLSConfig()
-	quicConfig := config.GenerateServerQUICConfig()
-	listener, err := quic.ListenAddr(addr, tlsConfig, quicConfig)
-	if err != nil {
-		return err
-	}
+func runt2q(listen, to string) error {
+	listenTcpAddr, err := net.ResolveTCPAddr("tcp4", listen)
 	toTcpAddr, err := net.ResolveTCPAddr("tcp4", to)
 	if err != nil {
-		return err
+		panic(err)
 	}
+	fmt.Printf("Listen TCP on: %s \n", listenTcpAddr.String())
 
+	lt, err := net.ListenTCP("tcp", listenTcpAddr)
+
+	tlsConf := config.GenerateClientTLSConfig()
+	quicConf := config.GenerateClientQUICConfig()
+	var sess quic.Connection = nil
 	for {
-		sess, err := listener.Accept(context.Background())
+		conn, err := lt.AcceptTCP()
 		if err != nil {
 			return err
 		}
-		//TODO error handling
-		go q2tHandleConn(sess, toTcpAddr)
+
+		// TODO
+		// and, if connection has closed
+		if sess == nil {
+			sess, err = quic.DialAddr(context.Background(), toTcpAddr.String(), tlsConf, quicConf)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("Connect QUIC to: %s \n", toTcpAddr.String())
+		}
+
+		// TODO error handling
+		go t2qHandleConn(conn, sess)
 	}
 	return nil
 }
 
-func q2tHandleConn(sess quic.Session, toTcpAddr *net.TCPAddr) error {
-	for {
-		stream, err := sess.AcceptStream(context.Background())
-		if err != nil {
-			return err
-		}
-		go q2tHandleStream(stream, toTcpAddr)
-	}
-}
-
-func q2tHandleStream(stream quic.Stream, toTcpAddr *net.TCPAddr) error {
-	fmt.Printf("Connect TCP to: %s \n", toTcpAddr.String())
-	conn, err := net.DialTCP("tcp", nil, toTcpAddr)
+func t2qHandleConn(conn *net.TCPConn, sess quic.Connection) error {
+	var stream quic.Stream
+	stream, err := sess.OpenStreamSync(context.Background())
 	if err != nil {
-		return err
+		panic(err)
 	}
 
 	eg := errgroup.Group{}
