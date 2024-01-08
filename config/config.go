de0eab0dc80f1cdf763ee53fae1b547300ee1fd4
oniyan <82947950+oniyan@users.noreply.github.com>
2024-01-08 19:07:11 +0900

    Update config.go

--- config/config.go
+++ config/config.go
@@ -15,17 +15,15 @@ const ALPN = "t2q2t"
 
 func GenerateClientQUICConfig() *quic.Config {
 	return &quic.Config{
-		MaxIdleTimeout: time.Duration(1) * time.Hour,
-		//KeepAlivePeriod:      true,
+		MaxIdleTimeout:       time.Duration(1) * time.Hour,
 		HandshakeIdleTimeout: time.Duration(5) * time.Second,
 	}
 }
 
 func GenerateServerQUICConfig() *quic.Config {
 	return &quic.Config{
-		MaxIdleTimeout: time.Duration(1) * time.Hour,
-		//KeepAlivePeriod:    true,
-		MaxIncomingStreams: 1024,
+		MaxIdleTimeout:       time.Duration(1) * time.Hour,
+		MaxIncomingStreams:   1024,
 	}
 }
 
@@ -36,29 +34,14 @@ func GenerateClientTLSConfig() *tls.Config {
 	}
 }
 
-func GenerateServerTLSConfig() *tls.Config {
+func GenerateServerTLSConfig(certFile, keyFile string) (*tls.Config, error) {
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return nil, err
+	}
+
 	return &tls.Config{
-		Certificates: []tls.Certificate{generateServerCertificate()},
+		Certificates: []tls.Certificate{cert},
 		NextProtos:   []string{ALPN},
-	}
-}
-
-func generateServerCertificate() tls.Certificate {
-	key, err := rsa.GenerateKey(rand.Reader, 1024)
-	if err != nil {
-		panic(err)
-	}
-	template := x509.Certificate{SerialNumber: big.NewInt(1)}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		panic(err)
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		panic(err)
-	}
-	return tlsCert
+	}, nil
 }
