031078b4baa081edc2a967bb99f1392defce4192
oniyan <82947950+oniyan@users.noreply.github.com>
2024-01-31 18:30:02 +0900

    Update config.go

--- config/config.go
+++ config/config.go
@@ -43,22 +43,13 @@ func GenerateServerTLSConfig() *tls.Config {
 	}
 }
 
-func generateServerCertificate() tls.Certificate {
-	key, err := rsa.GenerateKey(rand.Reader, 1024)
-	if err != nil {
-		panic(err)
-	}
-	template := x509.Certificate{SerialNumber: big.NewInt(1)}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		panic(err)
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		panic(err)
-	}
-	return tlsCert
+func GenerateServerTLSConfig(certFile, keyFile string) (*tls.Config, error) {
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+    	if err != nil {
+        	return nil, err
+    	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		NextProtos:   []string{ALPN},
+	}, nil
 }
